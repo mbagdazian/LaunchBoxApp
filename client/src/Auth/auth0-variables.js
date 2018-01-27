28db5ef6cd33ed01ca9f9709420d81d8ba7accea
ewok1138 <mbagdazian90@gmail.com>
2018-01-26 23:29:30 -0800

    added callback

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'launchboxapp.auth0.com',
   clientId: '7dFmBL4qa5a84ROZPcbOfDWR546dKnEc',
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'https://whispering-dawn-14333.herokuapp.com/'
 }
